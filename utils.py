a7f4617748957637ca9629eda5c4becf7eca087b
MasterAman24 <137294268+MasterAman24@users.noreply.github.com>
2025-09-05 19:40:47 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import base64
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as img_file:
+        encoded = base64.b64encode(img_file.read()).decode()
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{encoded}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }}
+        </style>
+        """, unsafe_allow_html=True
+    )
